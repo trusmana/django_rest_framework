f5ecaf701cd3012204fc02dee5dce19af3949eab
tedyrusmanawinangoen@gmail.com <tedyrusmanawinangoen.gmail.com>
2019-04-15 01:35:36 +0700

    Rest Api

--- api_example/urls.py
+++ api_example/urls.py
@@ -1,8 +1,14 @@
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.contrib import admin
 from django.views import generic
+from rest_framework import routers
+from api.views import SongView
+
+router = routers.DefaultRouter()
+router.register('song',SongView)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'',generic.TemplateView.as_view(template_name='index.html')),
+    url(r'^$',generic.TemplateView.as_view(template_name='inti.html')),
+    url(r'^api/', include(router.urls)),
 ]
