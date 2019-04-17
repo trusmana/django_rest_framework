7d8e5bc47362dc6988b6a462f8694ab699cbd1eb
tedyrusmanawinangoen@gmail.com <tedyrusmanawinangoen.gmail.com>
2019-04-18 03:04:23 +0700

    18 april 2019

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Song
+from .models import Song,Album
 
 class SongSerializer(serializers.ModelSerializer):
     class Meta:
         model = Song
+        fields = '__all__'
+
+class AlbumSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Album
         fields = '__all__'
--- api/views.py
+++ api/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .models import Song
-from .serializers import SongSerializer
+from .models import Song,Album
+from .serializers import SongSerializer,AlbumSerializer
 
 class SongView(viewsets.ModelViewSet):
     queryset = Song.objects.all()
     serializer_class = SongSerializer
+
+class AlbumView(viewsets.ModelViewSet):
+    queryset = Album.objects.all()
+    serializer_class = AlbumSerializer
